3cd38785e5de30ca08debf40db5f18dc9f4d0f45
Ray Yapari <ray.yapari@tokopedia.com>
2017-12-29 23:51:42 +0700

    add more pages

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,6 +1,16 @@
 import $ from 'jquery';
 
 $(document).ready(function () {
+    $('.trigger-show-password').click(function () {
+        $(this).toggleClass('fa-eye fa-eye-slash');
+
+        if ($(this).hasClass('fa-eye')) {
+            $('.ms-input__password').attr('type', 'text');
+        } else {
+            $('.ms-input__password').attr('type', 'password');
+        }
+    });
+
     /*Function for drawer*/
     $('.ms-burger').click(function () {
         $('body').css({
@@ -22,4 +32,26 @@ $(document).ready(function () {
 
     $('.ms-overlay').click(closeDrawer);
     /*end Function for drawer*/
+
+    /* Function for Upload File */
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            var img_id = $(input).attr('set-to');
+
+            reader.onload = function(e) {
+                $('#'+img_id).attr('src', e.target.result);
+                $('#'+img_id).css('visibility', 'visible');
+            }
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+
+    $('.ms-input__file').change(function() {
+        readURL(this);
+        $(this).parent().find('p, i').css('visibility','hidden');
+        $(this).css('z-index', 1);
+    });
+    /* end Function for Upload File */
 });
